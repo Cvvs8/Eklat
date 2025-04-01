0162e525ab4f25853a95de11ff324f0918775ef7
Cvvs8 <carlosvergaralcti@gmail.com>
2025-03-31 22:10:08 -0500

    r_sf

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, session, flash
+from flask import Flask, request, render_template, redirect, url_for, session, flash, send_file
 import bcrypt
 import json
 import os
